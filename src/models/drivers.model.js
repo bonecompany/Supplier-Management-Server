4a107099f2c7a3fdf6a59ad07c52857e27b8c88b
Anshid <muhammedanshidh.t@gmail.com>
2024-07-22 16:30:44 +0530

    Third Update

--- src/models/drivers.model.js
+++ src/models/drivers.model.js
@@ -1,1 +1,37 @@
-// driversmodel
+import mongoose, { Schema } from "mongoose";
+
+const drivers_schema = new Schema(
+    {
+        name: { type: String, required: true, index: true },
+
+        phone: { type: Number, require: [true, "enter mobile number"] },
+
+        address: { type: String, require: true },
+
+        password: { type: String, required: true },
+
+        refresh_token: { type: String },
+
+        vehicle_number : {type : String},
+
+        // avatar: { type: String },
+
+        suppliers : [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Suppliers"
+            }
+        ],
+        tappers : [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Tappers"
+            }
+        ]
+
+    },
+    { timestamps: true }
+
+)
+
+export const drivers = mongoose.model("Drivers", drivers_schema)
