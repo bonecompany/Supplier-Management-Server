5d7ab0979f66c4146eb3af5264e442cb47c78116
Anshid <muhammedanshidh.t@gmail.com>
2024-08-02 17:24:15 +0530

    02/08/2024

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,17 @@
-//admincontroller
-export const Registration = async (req, res) => {
-   return "Success";
-};
+import  async_handler  from "../utils/asyncHandler.js";
+// admin login
+
+const admin_login = async_handler(async (req,res) => {
+
+   const {username,password} = req.body
+
+   console.log(username)
+   console.log(password)
+
+await res.json(username)
+
+})
+
+export default {
+   admin_login
+}
