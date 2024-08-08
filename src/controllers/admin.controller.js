5a5f53659f989fe2db983e398575c20faefdb668
mkkpkidangayam <mkkpkdm@gmail.com>
2024-08-08 22:30:54 +0530

    Admin Login

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,7 +2,7 @@ import { Admin } from "../models/admin.model.js";
 import async_handler from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken'
 import bcrypt from "bcrypt";
-import {ApiError} from "../utils/ApiError.js";
+import { ApiError } from "../utils/ApiError.js";
 
 
 // create_admin-----------------------------------------
@@ -21,18 +21,17 @@ const admin_login = async_handler(async (req, res) => {
    const { username, password } = req.body
 
    const admin = await Admin.findOne({ username });
-   if (!admin) 
-      // return res.status(400).json({ error: 'Invalid username' });
-   return res.json(new ApiError(200, 'Invalid username' ))
+   if (!admin) return res.status(400).json({ error: 'Invalid username' });
+   // return res.json(new ApiError(200, 'Invalid username' ))
 
    const isMatch = await bcrypt.compare(password, admin.password);
    if (!isMatch) return res.status(400).json({ error: 'Incorrect password' });
 
-   const token = jwt.sign({ id: admin._id, isAdmin: true }, process.env.JWT_SECRET, { expiresIn: '1h' });
+   const adminToken = jwt.sign({ id: admin._id, isAdmin: true }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
    res.json({
       message: "Admin login successfully",
-      token
+      adminToken
    });
 })
 
